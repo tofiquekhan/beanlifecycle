4c8a44a7589bd144526dd91d00c869af1717c082
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2021-12-28 21:12:59 +0530

    Static Factory Method Bean Instantiation

--- src/myproject/beaninstantiation/beans/HelloBean.java
+++ src/myproject/beaninstantiation/beans/HelloBean.java
@@ -6,11 +6,16 @@ public class HelloBean {
 		System.out.println("HelloBean class loading......");
 	}
 	
-	private HelloBean() {
+	public  HelloBean() {
 		System.out.println("HelloBean class Instantiation..........");
 	}
 	
 	public String sayHello() {
 		return "Hello User!";
+	}
+	
+	public static HelloBean getInstance() {
+		System.out.println("Static Factory Method");
+		return new HelloBean();
 	}
 }
